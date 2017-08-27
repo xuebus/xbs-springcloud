5b13869cd5213d5c86ed358c19eee7a7d48b2838
yanjun.shi <yanjun.shi@zhaopin.com.cn>
2017-08-27 21:43:50 +0800

    test

--- xbs-service-a/src/main/java/com/zhaopin/AServiceApplication.java
+++ xbs-service-a/src/main/java/com/zhaopin/AServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 @SpringBootApplication
 public class AServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AServiceApplication.class, args);
     }
